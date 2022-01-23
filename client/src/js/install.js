7f352e5fd2db79926a722e320f7159bc58e28331
wwbarton <wbartonw5@gmail.com>
2022-01-22 21:59:44 -0700

    test

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,10 +1,8 @@
 const butInstall = document.getElementById("buttonInstall");
 
 window.addEventListener("beforeinstallprompt", (event) => {
-  // Store the triggered events
   window.deferredPrompt = event;
 
-  // Remove the hidden class from the button.
   butInstall.classList.toggle("hidden", false);
 });
 
@@ -15,16 +13,13 @@ butInstall.addEventListener("click", async () => {
     return;
   }
 
-  // Show prompt
   promptEvent.prompt();
 
-  // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
 
   butInstall.classList.toggle("hidden", true);
 });
 
 window.addEventListener("appinstalled", (event) => {
-  // Clear prompt
   window.deferredPrompt = null;
 });
